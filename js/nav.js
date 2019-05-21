574a256925b5bde5301a684660a65575ebcf9632
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-05-20 21:54:10 -0400

    New order to nav bar, hide search box, larger nav bar

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,19 @@
 let menu = document.querySelector('.hamburger');
-
+let searchBar = document.querySelector('input');
+let dropdown = document.querySelector('.dropdown-menu');
+console.log(dropdown);
 menu.addEventListener('click', (toggle));
 
 function toggle() {
     this.classList.toggle('is-active');
-    let dropdown = document.querySelector('.dropdown-menu');
+    console.log(dropdown)
     dropdown.classList.toggle('hidden-menu');
+    searchBar.classList.add('hidden-search');
 }
+
+let searchButton = document.querySelector('.search-icon');
+
+searchButton.addEventListener('click', () => {
+    searchBar.classList.toggle('hidden-search');
+    dropdown.classList.add('hidden-menu');
+});
